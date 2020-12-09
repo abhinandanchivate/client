5ca17b45b162dfcae4f18c9b6de9487e155d6d49
abhinandan chivate <abhinandanchivate2000@gmail.com>
2020-12-09 19:34:09 +0000

    hook

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,79 +1,75 @@
-import axios from "axios";
-import React, { Component } from "react";
 
-export default class Login extends Component {
-  constructor() {
-    super();
-    this.state = {
-      username: "",
-      password: "",
-    };
-  }
+import React, { useState } from 'react'
+import axios from 'axios'
+ const Login = () => {
 
-  onSubmit = (e) => {
+  
+//const [data,settterfunction]= useState({initail data})
+  const [formData,setFormData]= useState({username:'',password:''})
+  const {username,password} = formData;
+ const  handleChange = (event) => {
+   setFormData({...formData,[event.target.name]:event.target.value})
+  };
+  
+  const onSubmit = (e) => {
     e.preventDefault();
-    console.log("Final state: " + JSON.stringify(this.state));
+    console.log("Final state: " + username );
+    console.log("password"+password);
     const loginUser = {
-      username: this.state.username,
-      password: this.state.password,
+      username: username,
+      password: password,
     };
     axios
       .post("http://localhost:9050/api/auth/signin", loginUser)
       .then((response) =>{ console.log(response.data)
       
-      localStorage.setItem('data',response.data)
+      localStorage.setItem('data',JSON.stringify(response.data))
       })
       .catch((err) => console.log(err));
   };
 
-  handleChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  render() {
-    return (
-      <div className="login">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-8 m-auto">
-              <h1 className="display-4 text-center">Log In</h1>
-              <p className="lead text-center">
-                Sign in to your DevConnector account
-              </p>
-              <form onSubmit={this.onSubmit}>
-                <div className="form-group">
-                  <input
-                    type="text"
-                    className="form-control form-control-lg"
-                    placeholder="Username"
-                    name="username"
-                    value={this.state.username}
-                    onChange={this.handleChange}
-                  />
-                </div>
-                <div className="form-group">
-                  <input
-                    type="password"
-                    className="form-control form-control-lg"
-                    placeholder="Password"
-                    name="password"
-                    value={this.state.password}
-                    onChange={this.handleChange}
-                  />
-                </div>
-                <input
-                  type="submit"
-                  value="Login"
-                  className="btn btn-info btn-block mt-4"
-                />
-              </form>
+  return (
+    <div className="login">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8 m-auto">
+          <h1 className="display-4 text-center">Log In</h1>
+          <p className="lead text-center">
+            Sign in to your DevConnector account
+          </p>
+          <form onSubmit={onSubmit}>
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control form-control-lg"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
             </div>
-          </div>
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control form-control-lg"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+              />
+            </div>
+            <input
+              type="submit"
+              value="Login"
+              className="btn btn-info btn-block mt-4"
+            />
+          </form>
         </div>
       </div>
-    );
-  }
+    </div>
+  </div>
+
+  )
 }
 
+export default Login;
