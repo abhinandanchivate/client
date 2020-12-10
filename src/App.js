f3de15a82c9aa09ec9575291fe1df964a9edb58f
abhinandan chivate <abhinandanchivate2000@gmail.com>
2020-12-10 17:37:46 +0000

    props example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from './components/layouts/Header'
-import Landing from './components/layouts/Landing'
-import Footer from './components/layouts/Footer'
 
-import { BrowserRouter as Router, Route} from 'react-router-dom'
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
+import PostList from './components/layouts/PostList'
+
+const postList=[
+  {
+    id:1,
+    content : 'this world will be out of the pandemic soon',
+    user:'abhi'
+  },
+  {
+    id:2,
+    content : 'hello from abhi',
+    user:'advik'
+  },
+  {
+    id:3,
+    content : 'How r u',
+    user:'abhi'
+  },
+  {
+    id:4,
+    content : 'this world will be out of the pandemic soon',
+    user:'abhi'
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <Router>
-     <Header></Header>
-     <Route exact path="/" component={Landing}></Route>
-     <Route exact path="/login" component={Login}></Route>
-     <Route exact path="/register" component={Register}></Route>
-     <Footer></Footer>
-     </Router>
+     <PostList posts={postList}></PostList>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+{/* <Router>
+<Header></Header>
+<Route exact path="/" component={Landing}></Route>
+<Route exact path="/login" component={Login}></Route>
+<Route exact path="/register" component={Register}></Route>
+<Route exact path="/sample" component={sample}></Route>
+<Footer></Footer>
+</Router> */}
